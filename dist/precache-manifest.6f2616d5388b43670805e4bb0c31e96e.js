96a75bad6362a9b6af9400d5de696478495c4469
yoyoyo <594372929@qq.com>
2019-01-22 15:37:06 +0800

    瓦力提交 dist文件 拉取代码后请自行删除dist文件

---
+++ dist/precache-manifest.6f2616d5388b43670805e4bb0c31e96e.js
@@ -0,0 +1,314 @@
+self.__precacheManifest = [
+  {
+    "revision": "38584db789b775f0ec06",
+    "url": "/js/refund_argument.86ab419f.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "3e41496add3ba3a21819",
+    "url": "/css/refund_plan.760b5eba.css"
+  },
+  {
+    "revision": "c6bb8e1324ccf31c951c",
+    "url": "/css/_404.010729da.css"
+  },
+  {
+    "revision": "065c82278fa7cc6b2c56",
+    "url": "/css/app.e8fbe06f.css"
+  },
+  {
+    "revision": "065c82278fa7cc6b2c56",
+    "url": "/js/app.15d4baea.js"
+  },
+  {
+    "revision": "8e0b7e530ac60ac96646",
+    "url": "/css/card_check.dd32678e.css"
+  },
+  {
+    "revision": "8e0b7e530ac60ac96646",
+    "url": "/js/card_check.dca6aeb2.js"
+  },
+  {
+    "revision": "b44457fe386037e8f1f3",
+    "url": "/css/card_connection.c82e31a4.css"
+  },
+  {
+    "revision": "b44457fe386037e8f1f3",
+    "url": "/js/card_connection.315d6202.js"
+  },
+  {
+    "revision": "9b129ef90d63ba3a5fea",
+    "url": "/css/card_lookup.6607c745.css"
+  },
+  {
+    "revision": "9b129ef90d63ba3a5fea",
+    "url": "/js/card_lookup.74f77750.js"
+  },
+  {
+    "revision": "7e9e5b65610214fad972",
+    "url": "/css/card_usage.e84ae7f3.css"
+  },
+  {
+    "revision": "7e9e5b65610214fad972",
+    "url": "/js/card_usage.1a8ee2f4.js"
+  },
+  {
+    "revision": "de8bf5d4e57b8a73b3d5",
+    "url": "/css/chunk-vendors.0a14d1e9.css"
+  },
+  {
+    "revision": "de8bf5d4e57b8a73b3d5",
+    "url": "/js/chunk-vendors.4cb3f68e.js"
+  },
+  {
+    "revision": "065e36fe0c715b14b469",
+    "url": "/css/coupon_normal.34b21aaf.css"
+  },
+  {
+    "revision": "065e36fe0c715b14b469",
+    "url": "/js/coupon_normal.6d00820b.js"
+  },
+  {
+    "revision": "9c727afc9156aa02a1e3",
+    "url": "/css/coupon_telcom.c4d7b922.css"
+  },
+  {
+    "revision": "9c727afc9156aa02a1e3",
+    "url": "/js/coupon_telcom.5a7f0920.js"
+  },
+  {
+    "revision": "5ec4abd7e77d3e4533f7",
+    "url": "/css/find_plan.660f8340.css"
+  },
+  {
+    "revision": "5ec4abd7e77d3e4533f7",
+    "url": "/js/find_plan.322e1906.js"
+  },
+  {
+    "revision": "116d864b50a110768440",
+    "url": "/css/login.6509ca6e.css"
+  },
+  {
+    "revision": "116d864b50a110768440",
+    "url": "/js/login.2e2407b0.js"
+  },
+  {
+    "revision": "aa302674a3455b9595c2",
+    "url": "/css/lookup.6e64b1a2.css"
+  },
+  {
+    "revision": "aa302674a3455b9595c2",
+    "url": "/js/lookup.a43d494e.js"
+  },
+  {
+    "revision": "94040406c686deb999b8",
+    "url": "/css/plan_list.b46244fc.css"
+  },
+  {
+    "revision": "94040406c686deb999b8",
+    "url": "/js/plan_list.e8b0743e.js"
+  },
+  {
+    "revision": "b1503f9dfcb86532a04d",
+    "url": "/css/question.d7781f4c.css"
+  },
+  {
+    "revision": "b1503f9dfcb86532a04d",
+    "url": "/js/question.ade986a7.js"
+  },
+  {
+    "revision": "3eaff7bb20a6197c8f6c",
+    "url": "/css/real_name.49362bec.css"
+  },
+  {
+    "revision": "3eaff7bb20a6197c8f6c",
+    "url": "/js/real_name.48afcd22.js"
+  },
+  {
+    "revision": "ce3f87e0f19602ebe477",
+    "url": "/css/recharge.dfe5a0cb.css"
+  },
+  {
+    "revision": "ce3f87e0f19602ebe477",
+    "url": "/js/recharge.7c6d555e.js"
+  },
+  {
+    "revision": "93d0de102dc5644a53da",
+    "url": "/css/recharge_callback.a107fbe0.css"
+  },
+  {
+    "revision": "93d0de102dc5644a53da",
+    "url": "/js/recharge_callback.dee0271e.js"
+  },
+  {
+    "revision": "f5d4d3b86d21b909c67d",
+    "url": "/css/refund_applying.3c632630.css"
+  },
+  {
+    "revision": "f5d4d3b86d21b909c67d",
+    "url": "/js/refund_applying.04822160.js"
+  },
+  {
+    "revision": "38584db789b775f0ec06",
+    "url": "/css/refund_argument.6891e35e.css"
+  },
+  {
+    "revision": "6b96305100aab27bc9d0",
+    "url": "/js/Layout.11db5899.js"
+  },
+  {
+    "revision": "c6bb8e1324ccf31c951c",
+    "url": "/js/_404.c901f7a6.js"
+  },
+  {
+    "revision": "3e41496add3ba3a21819",
+    "url": "/js/refund_plan.bfcddeec.js"
+  },
+  {
+    "revision": "1ba80927decc8cc021b8",
+    "url": "/css/revoke_plan.3e9f1792.css"
+  },
+  {
+    "revision": "1ba80927decc8cc021b8",
+    "url": "/js/revoke_plan.b8d28c41.js"
+  },
+  {
+    "revision": "7bddc7074411e4770bdf",
+    "url": "/css/speedup_500.b6944bad.css"
+  },
+  {
+    "revision": "7bddc7074411e4770bdf",
+    "url": "/js/speedup_500.e873c2c4.js"
+  },
+  {
+    "revision": "f230b3397505f1a456f8",
+    "url": "/css/speedup_80.f3747c99.css"
+  },
+  {
+    "revision": "f230b3397505f1a456f8",
+    "url": "/js/speedup_80.09920523.js"
+  },
+  {
+    "revision": "7d71dfa29a8771ae2ebf",
+    "url": "/css/to_tb.5f146892.css"
+  },
+  {
+    "revision": "7d71dfa29a8771ae2ebf",
+    "url": "/js/to_tb.5f6c3d05.js"
+  },
+  {
+    "revision": "e4b411644706c29765eea3ffdd85ae39",
+    "url": "/fonts/SourceHanSansCNRegular.e4b41164.woff"
+  },
+  {
+    "revision": "834b022ab9ab0e6063904af32274e777",
+    "url": "/fonts/SourceHanSansCNRegular.834b022a.eot"
+  },
+  {
+    "revision": "f9509c5a10be7daaf1fa01922a7c6bc2",
+    "url": "/fonts/SourceHanSansCNRegular.f9509c5a.ttf"
+  },
+  {
+    "revision": "da0808e9fe85be160aab69c82dd8de1c",
+    "url": "/img/SourceHanSansCNRegular.da0808e9.svg"
+  },
+  {
+    "revision": "f5f322478286641bd9887a155e35f222",
+    "url": "/img/btn.f5f32247.png"
+  },
+  {
+    "revision": "d140650fa64e0f1fc771610dba62aed7",
+    "url": "/img/bg.d140650f.png"
+  },
+  {
+    "revision": "1d7515ee61c3f659227318b4050955ed",
+    "url": "/img/mobile_sim@2x.1d7515ee.png"
+  },
+  {
+    "revision": "0f980b79d9a52702004de9f652a68835",
+    "url": "/img/telcom_sim@2x.0f980b79.png"
+  },
+  {
+    "revision": "98141226a7e992e3072a280746a259c4",
+    "url": "/img/mobile_sim@3x.98141226.png"
+  },
+  {
+    "revision": "0c88168b284b92c37f3a873f26a97d69",
+    "url": "/img/bg.0c88168b.jpg"
+  },
+  {
+    "revision": "9e02095f31a07b972244f1c714c1dfe7",
+    "url": "/img/telcom_sim@3x.9e02095f.png"
+  },
+  {
+    "revision": "3f342771b9b81ab929e0fbcbcd0e6533",
+    "url": "/img/bg-check-wrap@3x.3f342771.png"
+  },
+  {
+    "revision": "9713b21819c4019befecd095069977df",
+    "url": "/img/bg-check-wrap@2x.9713b218.png"
+  },
+  {
+    "revision": "3d0fc424b703032ca595090e99f8354b",
+    "url": "/img/login_bg@2x.3d0fc424.png"
+  },
+  {
+    "revision": "11000d4e510a6df565eb69ba6f2c1dfc",
+    "url": "/img/login_bg@3x.11000d4e.png"
+  },
+  {
+    "revision": "5a3428653945c17e1c146efeee81c987",
+    "url": "/img/scanTop.5a342865.png"
+  },
+  {
+    "revision": "824594523c01e48028f2b7eb4261134d",
+    "url": "/img/unicom-logo.82459452.svg"
+  },
+  {
+    "revision": "113f99e2189c2ca49db2168754fc2eb5",
+    "url": "/img/mobile-logo.113f99e2.png"
+  },
+  {
+    "revision": "27158aa79eb0d4b89f317d076a801ca3",
+    "url": "/img/coupon-log.27158aa7.png"
+  },
+  {
+    "revision": "55d3f2f09efe17db6485d4c9f8f0c3b1",
+    "url": "/img/icon-recharge_20181001.55d3f2f0.svg"
+  },
+  {
+    "revision": "5f1f34037094c0613bccafa2ddd8cb3b",
+    "url": "/img/telecom-logo.5f1f3403.svg"
+  },
+  {
+    "revision": "4bdbe7dbdb6587c886a8290593100145",
+    "url": "/img/recharge_callback_20181126.4bdbe7db.jpg"
+  },
+  {
+    "revision": "aecc9531f2c3b1fd45338e7bf5084384",
+    "url": "/img/bg_no_recharge.aecc9531.svg"
+  },
+  {
+    "revision": "275be51ae57c486bce58257febd6589f",
+    "url": "/img/bg_no_plan.275be51a.svg"
+  },
+  {
+    "revision": "79c97796a3112d204fdfffd70985807a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b96305100aab27bc9d0",
+    "url": "/css/Layout.c59a8b7e.css"
+  },
+  {
+    "revision": "86c422d8c98b247235d04b49fb1c4b05",
+    "url": "/MP_verify_bKSdn2qZlUSULliu.txt"
+  },
+  {
+    "revision": "00bbf6b458bf7ea6bd01f443eb3045d9",
+    "url": "/MP_verify_4r3DVk3KUjgWsQsG.txt"
+  }
+];
