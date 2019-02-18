8b9efc5670df9e833c86fff7f6fd1f6d285e67ef
yoyoyo <594372929@qq.com>
2019-02-18 15:27:29 +0800

    请求匹配错误提示

--- src/http/index.js
+++ src/http/index.js
@@ -23,8 +23,7 @@ axios.interceptors.request.use(function (config) {
 axios.interceptors.response.use(function (response) {
     return response.data
 },err => {
-
-    if(Object.prototype.toString.call(err)=='[object object]'){
+    if(Object.prototype.toString.call(err)=='[object object]' || Object.prototype.toString.call(err)=='[object Error]'){
         return Promise.resolve({
             state:0,
             msg:'服务出小差啦('+ err.response.status+')'
