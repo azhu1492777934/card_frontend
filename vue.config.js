514ed87d9566e2f58d56b99abb3709244f9d85e9
yoyoyo <594372929@qq.com>
2019-06-18 17:24:51 +0800

    关闭 prefetch

--- vue.config.js
+++ vue.config.js
@@ -1,75 +1,67 @@
-/*
-* payCenter:支付中心
-* accountCenter:用户中心
-* card_server:后台
-* card_server2:包月套餐解约
-* */
-
-
-var domain_test = {
-    card_server : 'https://cardserver_test.china-m2m.com',
-    accountCenter : 'http://uc-test.api.china-m2m.com',
-    card_server2:'http://cardserver_mac.china-m2m.com/'
-}
-var domain_production = {
-    card_server : 'https://cardserver_test.china-m2m.com',
-    accountCenter : 'http://uc.api.china-m2m.com',
-}
-
 const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
-    configureWebpack:config=>{
-        if(process.env.NODE_ENV === 'production'){
-            return{
-                plugins:[new CompressionPlugin({
-                    test:/\.js$|\.html$|\.css/,
-                    threshold:1024, //对超过1k的数据进行压缩
-                    deleteOriginalAssets:false//是否删除源文件
-                })]
-            }
-        }
-    },
-    devServer: {
-        port:8080,
-        disableHostCheck:true,
-        proxy: {
-            '/api': {
-                target:'http://cardserver_test.china-m2m.com',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            },
-            '/accountCenter':{
-                target:'http://uc-test.api.china-m2m.com',
-                changeOrigin:true,
-                pathRewrite:{
-                    '^/accountCenter':''
-                }
-            },
-            '/iot':{
-                target:'https://iottest.china-m2m.com',
-                changeOrigin:true,
-                pathRewrite:{   
-                    '^/iot':''
-                }
-            },
-            '/releaseApi':{
-                target:"http://cardserver_mac.china-m2m.com",
-                changeOrigin:true,
-                pathRewrite:{
-                    '^/releaseApi':""
-                }
-            },
-            '/kachiApi':{
-                target:"http://kachitest.china-m2m.com",
-                changeOrigin:true,
-                pathRewrite:{
-                    '^/kachiApi':""
-                }
-            },
-            
-        }
+  chainWebpack: config => {
+    // 移除prefetch 插件
+    config.plugins.delete('prefetch')
+    // 另一种做法
+    // config.plugin('prefetch').tap(options => {
+    //   options[0].fileBlacklist = options[0].fileBlacklist || [];
+    //   options[0].fileBlacklist.push(/myasynncRoute(.)+?\.js$/);
+    //   return options
+    // })
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [new CompressionPlugin({
+          test: /\.js$|\.html$|\.css/,
+          threshold: 1024, //对超过1k的数据进行压缩
+          deleteOriginalAssets: false//是否删除源文件
+        })]
+      }
     }
+  },
+  devServer: {
+    port: 8080,
+    disableHostCheck: true,
+    proxy: {
+      '/api': {
+        target: 'http://cardserver_test.china-m2m.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      '/accountCenter': {
+        target: 'http://uc-test.api.china-m2m.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/accountCenter': ''
+        }
+      },
+      '/iot': {
+        target: 'https://iottest.china-m2m.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/iot': ''
+        }
+      },
+      '/releaseApi': {
+        target: "http://cardserver_mac.china-m2m.com",
+        changeOrigin: true,
+        pathRewrite: {
+          '^/releaseApi': ""
+        }
+      },
+      '/kachiApi': {
+        target: "http://kachitest.china-m2m.com",
+        changeOrigin: true,
+        pathRewrite: {
+          '^/kachiApi': ""
+        }
+      },
+
+    }
+  }
 }
