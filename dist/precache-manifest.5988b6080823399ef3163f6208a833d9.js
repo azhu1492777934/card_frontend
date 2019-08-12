587346a912b1ed2fcd2982070e69597a90b2e7e7
ttt <460312793@qq.com>
2019-08-12 14:49:52 +0800

    退款规则更改

--- dist/precache-manifest.5988b6080823399ef3163f6208a833d9.js
+++ dist/precache-manifest.5988b6080823399ef3163f6208a833d9.js
@@ -4,16 +4,16 @@ self.__precacheManifest = [
     "url": "/js/recharge_callback.d8237c4b.js"
   },
   {
-    "revision": "685f36a61c6b9388424d",
-    "url": "/css/Layout.83a36cb0.css"
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9feffacd2b7a96a33fbf",
+    "url": "/css/recharge_wrapper.61f8e218.css"
   },
   {
     "revision": "be40bca99df105cbae95",
     "url": "/css/Layout~card_usage.cb0d2a22.css"
-  },
-  {
-    "revision": "be40bca99df105cbae95",
-    "url": "/js/Layout~card_usage.bcf0ce41.js"
   },
   {
     "revision": "78214ba34c5f1df936aa",
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/js/Not_fund.a6629d31.js"
   },
   {
-    "revision": "8b83efd45991a39d2ad9",
+    "revision": "fce83459594f928b8e6e",
     "url": "/css/app.72184c26.css"
   },
   {
-    "revision": "8b83efd45991a39d2ad9",
-    "url": "/js/app.aeac958f.js"
+    "revision": "fce83459594f928b8e6e",
+    "url": "/js/app.4f4bd8a9.js"
   },
   {
     "revision": "9f5767044d21c4e964bd",
@@ -436,8 +436,8 @@ self.__precacheManifest = [
     "url": "/js/Layout.7e2130a0.js"
   },
   {
-    "revision": "9feffacd2b7a96a33fbf",
-    "url": "/css/recharge_wrapper.61f8e218.css"
+    "revision": "be40bca99df105cbae95",
+    "url": "/js/Layout~card_usage.bcf0ce41.js"
   },
   {
     "revision": "9feffacd2b7a96a33fbf",
@@ -452,12 +452,12 @@ self.__precacheManifest = [
     "url": "/js/refund_applying.5b1dac3d.js"
   },
   {
-    "revision": "6d4a8182771ea3a147fc",
+    "revision": "0da7ef1a87ff4f4a2add",
     "url": "/css/refund_argument.69661661.css"
   },
   {
-    "revision": "6d4a8182771ea3a147fc",
-    "url": "/js/refund_argument.472e4804.js"
+    "revision": "0da7ef1a87ff4f4a2add",
+    "url": "/js/refund_argument.e3025ae6.js"
   },
   {
     "revision": "20c3adedf4f2e93c7b24",
@@ -572,48 +572,12 @@ self.__precacheManifest = [
     "url": "/fonts/element-icons.535877f5.woff"
   },
   {
-    "revision": "91e1d2771efacd45e5e465d9a001f197",
-    "url": "/img/liantong.91e1d277.png"
-  },
-  {
-    "revision": "4a0cab0f33beb48ea985714d1b91fef7",
-    "url": "/img/dianxin.4a0cab0f.png"
-  },
-  {
-    "revision": "657033323334e91efb0011bbc18f8c04",
-    "url": "/img/yidong.65703332.png"
-  },
-  {
-    "revision": "8da3b83b73e44e54d6be4b2522bcb1f2",
-    "url": "/img/pkgBg2.8da3b83b.png"
-  },
-  {
-    "revision": "d3b42651f9f5048baeffb50c800913ec",
-    "url": "/img/pkgBg1.d3b42651.png"
-  },
-  {
-    "revision": "d461d18e42a310bd709b2ce97d159153",
-    "url": "/img/pkgBg3.d461d18e.png"
-  },
-  {
-    "revision": "6dedd4943e4c5848638196fb6f0c4145",
-    "url": "/img/bg.6dedd494.png"
-  },
-  {
     "revision": "0078f03c08066d51d64b92a13e194ebb",
     "url": "/img/balanceRefund.0078f03c.png"
   },
   {
     "revision": "af3ed0b3236bbfdbc3ad61b8d41c3dec",
     "url": "/img/changeBg.af3ed0b3.png"
-  },
-  {
-    "revision": "42c0fbd4915870e5e60452fd0ef2cbec",
-    "url": "/img/orderTopBg@3x.42c0fbd4.png"
-  },
-  {
-    "revision": "42c0fbd4915870e5e60452fd0ef2cbec",
-    "url": "/img/orderTopBg@2x.42c0fbd4.png"
   },
   {
     "revision": "fff5af4325410621aeec4e60b3b92d9b",
@@ -624,12 +588,32 @@ self.__precacheManifest = [
     "url": "/img/mifi_binding_bg@2x.fff5af43.png"
   },
   {
+    "revision": "42c0fbd4915870e5e60452fd0ef2cbec",
+    "url": "/img/orderTopBg@3x.42c0fbd4.png"
+  },
+  {
+    "revision": "42c0fbd4915870e5e60452fd0ef2cbec",
+    "url": "/img/orderTopBg@2x.42c0fbd4.png"
+  },
+  {
+    "revision": "6e5cee7393060b0ec3dcd587c2518b94",
+    "url": "/img/bg_test.6e5cee73.png"
+  },
+  {
     "revision": "b899232b79ad9962c5ab5572775a0400",
     "url": "/img/card_index_bg@3x.b899232b.png"
   },
   {
     "revision": "b899232b79ad9962c5ab5572775a0400",
     "url": "/img/card_index_bg@2x.b899232b.png"
+  },
+  {
+    "revision": "2c51a1e53c1ea0b35086d8d7261b4933",
+    "url": "/img/bg_network@3x.2c51a1e5.png"
+  },
+  {
+    "revision": "32d70c8fc305e3647098f3de0c71b618",
+    "url": "/img/bg_network@2x.32d70c8f.png"
   },
   {
     "revision": "f5f322478286641bd9887a155e35f222",
@@ -646,6 +630,14 @@ self.__precacheManifest = [
   {
     "revision": "af8bbdfa0a90c4eed4563f8c06ce5ff8",
     "url": "/img/android@2x.af8bbdfa.png"
+  },
+  {
+    "revision": "3f342771b9b81ab929e0fbcbcd0e6533",
+    "url": "/img/bg-check-wrap@3x.3f342771.png"
+  },
+  {
+    "revision": "9713b21819c4019befecd095069977df",
+    "url": "/img/bg-check-wrap@2x.9713b218.png"
   },
   {
     "revision": "1d7515ee61c3f659227318b4050955ed",
@@ -668,22 +660,6 @@ self.__precacheManifest = [
     "url": "/img/telcom_sim@3x.9e02095f.png"
   },
   {
-    "revision": "2c51a1e53c1ea0b35086d8d7261b4933",
-    "url": "/img/bg_network@3x.2c51a1e5.png"
-  },
-  {
-    "revision": "32d70c8fc305e3647098f3de0c71b618",
-    "url": "/img/bg_network@2x.32d70c8f.png"
-  },
-  {
-    "revision": "3f342771b9b81ab929e0fbcbcd0e6533",
-    "url": "/img/bg-check-wrap@3x.3f342771.png"
-  },
-  {
-    "revision": "9713b21819c4019befecd095069977df",
-    "url": "/img/bg-check-wrap@2x.9713b218.png"
-  },
-  {
     "revision": "b70eddccd257b21777e71bb64c83b876",
     "url": "/img/bg.b70eddcc.jpeg"
   },
@@ -702,34 +678,6 @@ self.__precacheManifest = [
   {
     "revision": "11000d4e510a6df565eb69ba6f2c1dfc",
     "url": "/img/login_bg@3x.11000d4e.png"
-  },
-  {
-    "revision": "014d354b9b25b67ae95857ba17e2f4cc",
-    "url": "/img/activityPage.014d354b.png"
-  },
-  {
-    "revision": "2a67b73145562669220aa4639e1ad58f",
-    "url": "/img/icon3.2a67b731.png"
-  },
-  {
-    "revision": "813b5319e378dafdda952c16ad89906b",
-    "url": "/img/icon2.813b5319.png"
-  },
-  {
-    "revision": "01355f175bd20423ce45a838c1a998a9",
-    "url": "/img/bg.01355f17.png"
-  },
-  {
-    "revision": "6e5cee7393060b0ec3dcd587c2518b94",
-    "url": "/img/bg_test.6e5cee73.png"
-  },
-  {
-    "revision": "a859e9ca4606d567c42b9c7bcd4fdce9",
-    "url": "/img/iccid_bg@2x.a859e9ca.png"
-  },
-  {
-    "revision": "fa9050c4e9870e8fc0cd184f8515af4a",
-    "url": "/img/iccid_bg@3x.fa9050c4.png"
   },
   {
     "revision": "114a6e12b42b9b48586dd341105188fe",
@@ -754,6 +702,58 @@ self.__precacheManifest = [
   {
     "revision": "be0f1e33b2553e71b117d3d3f838d32f",
     "url": "/img/invaildedOrder@2x.be0f1e33.png"
+  },
+  {
+    "revision": "91e1d2771efacd45e5e465d9a001f197",
+    "url": "/img/liantong.91e1d277.png"
+  },
+  {
+    "revision": "4a0cab0f33beb48ea985714d1b91fef7",
+    "url": "/img/dianxin.4a0cab0f.png"
+  },
+  {
+    "revision": "657033323334e91efb0011bbc18f8c04",
+    "url": "/img/yidong.65703332.png"
+  },
+  {
+    "revision": "8da3b83b73e44e54d6be4b2522bcb1f2",
+    "url": "/img/pkgBg2.8da3b83b.png"
+  },
+  {
+    "revision": "d3b42651f9f5048baeffb50c800913ec",
+    "url": "/img/pkgBg1.d3b42651.png"
+  },
+  {
+    "revision": "d461d18e42a310bd709b2ce97d159153",
+    "url": "/img/pkgBg3.d461d18e.png"
+  },
+  {
+    "revision": "014d354b9b25b67ae95857ba17e2f4cc",
+    "url": "/img/activityPage.014d354b.png"
+  },
+  {
+    "revision": "6dedd4943e4c5848638196fb6f0c4145",
+    "url": "/img/bg.6dedd494.png"
+  },
+  {
+    "revision": "2a67b73145562669220aa4639e1ad58f",
+    "url": "/img/icon3.2a67b731.png"
+  },
+  {
+    "revision": "813b5319e378dafdda952c16ad89906b",
+    "url": "/img/icon2.813b5319.png"
+  },
+  {
+    "revision": "01355f175bd20423ce45a838c1a998a9",
+    "url": "/img/bg.01355f17.png"
+  },
+  {
+    "revision": "a859e9ca4606d567c42b9c7bcd4fdce9",
+    "url": "/img/iccid_bg@2x.a859e9ca.png"
+  },
+  {
+    "revision": "fa9050c4e9870e8fc0cd184f8515af4a",
+    "url": "/img/iccid_bg@3x.fa9050c4.png"
   },
   {
     "revision": "3603a35a49dbb8ad94ec1e57c4dad229",
@@ -844,12 +844,12 @@ self.__precacheManifest = [
     "url": "/img/06.999589aa.png"
   },
   {
-    "revision": "17386af97856de645be5d8913356f7ff",
+    "revision": "594d2b647cb927610b2904f986c00ea5",
     "url": "/index.html"
   },
   {
-    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
-    "url": "/robots.txt"
+    "revision": "685f36a61c6b9388424d",
+    "url": "/css/Layout.83a36cb0.css"
   },
   {
     "revision": "9ce16f441b39b493af8b96d7e97cce22",
