44340418cfd64cfc84749c22091c173aa036eeea
Denton <1015384519@qq.com>
2020-07-22 14:59:21 +0800

    add：微信jdk添加ready函数

--- src/views/card/lookup.vue
+++ src/views/card/lookup.vue
@@ -358,19 +358,22 @@
       scanIccid() {
         let _this = this;
         if (this.client_type === 'wechat') {
-          this.wx.scanQRCode({
-            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-            scanType: ["barCode", "qrCode"], // 可以指定扫二维码还是一维码，默认二者都有
-            success: function (res) {
-              var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-              result.indexOf(',') > -1 ? result = result.substr(result.indexOf(',') + 1, result.length) : result;
-              _this.iccid = (result.replace(/\s*/g, ""));
-              _this.processCheckIccid(_this.iccid);
-            },
-            fail: function(err) {
-              Notify({message: '暂不支持，敬请原谅！'});
-            }
-          });
+          this.wx.ready(function(){
+            console.log(_this.wx)
+            _this.wx.scanQRCode({
+              needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+              scanType: ["barCode", "qrCode"], // 可以指定扫二维码还是一维码，默认二者都有
+              success: function (res) {
+                var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                result.indexOf(',') > -1 ? result = result.substr(result.indexOf(',') + 1, result.length) : result;
+                _this.iccid = (result.replace(/\s*/g, ""));
+                _this.processCheckIccid(_this.iccid);
+              },
+              fail: function(err) {
+                Notify({message: '暂不支持，敬请原谅！'});
+              }
+            });
+          })
         } else if (this.client_type === 'alipay') {
           ap.scan(function (res) {
             let result = res.code; // 当needResult 为 1 时，扫码返回的结果
